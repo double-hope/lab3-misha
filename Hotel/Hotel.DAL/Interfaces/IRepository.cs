aaa6ca6fabe9a7e507b9a3433a0e3949e2a3b776
double-hope <nadia.prohorchuk@gmail.com>
2024-01-05 13:12:06 +0200

    small fixes

--- Hotel/Hotel.DAL/Interfaces/IRepository.cs
+++ Hotel/Hotel.DAL/Interfaces/IRepository.cs
@@ -5,7 +5,6 @@ namespace Hotel.DAL.Interfaces
     public interface IRepository<T> where T : class
     {
         Task<IEnumerable<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
-
         Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> filter);
         Task AddAsync(T entity);
         Task AddRangeAsync(IEnumerable<T> entities);
